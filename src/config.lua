94e8232107963ba7283f075d79e406d8f3495103
nirunakahokaku-tech <nirunakahokaku@gmail.com>
2026-07-10 03:17:57 +0700

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -1,6 +1,7 @@
 local Config = {
     Enabled = true,
-    Depth = 150,
+    Depth = 150,             -- Độ sâu để đè bẹp cái đầu thật trên màn hình của BẠN
+    SacrificeItem = "Robloxclassicred", -- TÊN PHỤ KIỆN TRÊN AVATAR CỦA MÀY ĐỂ BẺ KHỚP (Ví dụ: "PalHair")
     ToggleKey = "H"
 }
 return Config
