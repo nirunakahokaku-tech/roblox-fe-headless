95cc053b664ddc73e099cfd8c2aca3aa8616ce56
nirunakahokaku-tech <nirunakahokaku@gmail.com>
2026-07-10 03:03:59 +0700

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -1,7 +1,6 @@
 local Config = {
     Enabled = true,
-    Depth = -20,             -- Độ sâu an toàn, ẩn hoàn toàn đầu và không bị chạm giới hạn Void
+    Depth = 150,
     ToggleKey = "H"
 }
-
 return Config
