97755c4447d63cd305433c9a98b8788d8b27fe5e
nirunakahokaku-tech <nirunakahokaku@gmail.com>
2026-07-10 02:31:52 +0700

    Create config.lua

---
+++ src/config.lua
@@ -0,0 +1,7 @@
+local Config = {
+    Enabled = true,          -- Tự động kích hoạt khi chạy script
+    Depth = -1000,           -- Độ sâu đẩy khớp đầu xuống dưới (FE hoạt động ở mọi khoảng cách sâu)
+    ToggleKey = "H"          -- Phím tắt để bật/tắt nhanh Headless
+}
+
+return Config
