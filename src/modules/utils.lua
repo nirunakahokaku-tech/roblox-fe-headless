96620cf82f24b85725fc24db9c96bdc86c22eadd
nirunakahokaku-tech <nirunakahokaku@gmail.com>
2026-07-10 02:30:04 +0700

    Create utils.lua for character utility functions
    Added utility functions for character handling in Roblox.

---
+++ src/modules/utils.lua
@@ -0,0 +1,29 @@
+local Utils = {}
+
+function Utils.waitForCharacter(player)
+    local character = player.Character or player.CharacterAdded:Wait()
+    local humanoid = character:WaitForChild("Humanoid", 10)
+    local head = character:WaitForChild("Head", 10)
+    
+    if humanoid and head then
+        return character, humanoid, head
+    end
+    return nil
+end
+
+function Utils.getNeckJoint(character, humanoid)
+    if humanoid.RigType == Enum.HumanoidRigType.R6 then
+        local torso = character:WaitForChild("Torso", 5)
+        if torso then
+            return torso:WaitForChild("Neck", 5)
+        end
+    elseif humanoid.RigType == Enum.HumanoidRigType.R15 then
+        local head = character:WaitForChild("Head", 5)
+        if head then
+            return head:WaitForChild("Neck", 5)
+        end
+    end
+    return character:FindFirstChild("Neck", true)
+end
+
+return Utils
