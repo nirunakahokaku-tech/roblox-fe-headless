975b45f78570b3ae897057959e44a07b7b056af9
nirunakahokaku-tech <nirunakahokaku@gmail.com>
2026-07-10 02:20:15 +0700

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,2 @@
+-- Đường dẫn load trực tiếp từ GitHub của bạn sau khi bạn upload lên repository
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TÊN_USER_GITHUB/TÊN_KHO_LƯU_TRỮ/main/build/bundled.lua"))()
