0ce67ecd1cc65f776f4d535b677001783b45d2c2
nirunakahokaku-tech <nirunakahokaku@gmail.com>
2026-07-10 02:37:32 +0700

    Update GitHub URL for loading bundled.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,1 @@
--- Đường dẫn load trực tiếp từ GitHub của bạn sau khi bạn upload lên repository
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TÊN_USER_GITHUB/TÊN_KHO_LƯU_TRỮ/main/build/bundled.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nirunakahokaku-tech/roblox-fe-headless/main/build/bundled.lua"))()
